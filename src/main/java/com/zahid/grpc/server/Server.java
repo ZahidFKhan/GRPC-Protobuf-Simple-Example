b057b6ec245f37c6503717b1966d7320e6d4ae44
Zahid_Khan <xahiidkhan@gmail.com>
2024-10-27 19:04:04 +0530

    Update the pom.xml and add the server code.

--- src/main/java/com/zahid/grpc/server/Server.java
+++ src/main/java/com/zahid/grpc/server/Server.java
@@ -1,6 +1,6 @@
 package com.zahid.grpc.server;
 
-
+import com.github.javafaker.Faker;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import org.lognet.springboot.grpc.GRpcService;
@@ -11,26 +11,60 @@ import org.zahid.models.StudentServiceGrpc;
 @GRpcService
 @Slf4j
 public class Server extends StudentServiceGrpc.StudentServiceImplBase {
-    @Override
-    public void serverSideStreaming(StudentRequest request, StreamObserver<StudentResponse> responseObserver) {
+  @Override
+  public void serverSideStreaming(
+      StudentRequest request, StreamObserver<StudentResponse> responseObserver) {
+    log.info("Received input from Client: {}", request);
+    for (int loop = 0; loop < 50; loop++) {
+      final var fakerInstance = Faker.instance();
+      final StudentResponse nextStudent =
+          StudentResponse.newBuilder()
+              .setName(fakerInstance.name().firstName())
+              .setAge(fakerInstance.number().numberBetween(18, 60))
+              .build();
 
+      log.info("Ready to receive : {}", nextStudent);
+      responseObserver.onNext(nextStudent);
     }
+    responseObserver.onCompleted();
+  }
 
-    @Override
-    public StreamObserver<StudentRequest> clientSideStreaming(StreamObserver<StudentResponse> responseObserver) {
-    }
+  @Override
+  public StreamObserver<StudentRequest> clientSideStreaming(
+      StreamObserver<StudentResponse> responseObserver) {
+    return new StreamObserver<>() {
+      @Override
+      public void onNext(StudentRequest value) {
+        log.info("Request received : {}", value);
+      }
 
-    @Override
-    public StreamObserver<StudentRequest> biDirectionalStream(StreamObserver<StudentResponse> responseObserver) {
-    }
+      @Override
+      public void onError(Throwable t) {
+        log.error("Request received : {}", t.getMessage());
+      }
 
-    @Override
-    public void getStudent(StudentRequest request, StreamObserver<StudentResponse> responseObserver) {
-        final var builder = StudentResponse.newBuilder();
-        final var zahid = builder.setName("Zahid").setAge(20).build();
-
-        responseObserver.onNext(zahid);
+      @Override
+      public void onCompleted() {
+        responseObserver.onNext(StudentResponse.getDefaultInstance());
         responseObserver.onCompleted();
-        log.info("Response sent");
-    }
+        log.info("Request completed");
+      }
+    };
+  }
+
+  @Override
+  public StreamObserver<StudentRequest> biDirectionalStream(
+      StreamObserver<StudentResponse> responseObserver) {
+    return null;
+  }
+
+  @Override
+  public void getStudent(StudentRequest request, StreamObserver<StudentResponse> responseObserver) {
+    final var builder = StudentResponse.newBuilder();
+    final var zahid = builder.setName("Zahid").setAge(20).build();
+
+    responseObserver.onNext(zahid);
+    responseObserver.onCompleted();
+    log.info("Response sent");
+  }
 }
